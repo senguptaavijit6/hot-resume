3fc4b308382cad8752f5c739375eb87cc7b3754b
Abhijit Sengupta <senguptaavijit6@gmail.com>
2025-12-03 10:14:23 +0530

    ID checking for validity is complete

--- server/src/middlewares/auth/strategies/google.ts
+++ server/src/middlewares/auth/strategies/google.ts
@@ -1,26 +1,36 @@
 import passport from 'passport'
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
 import authModel from '../../../modules/auth/auth.model.js'
+import AuthRepo from '../../../modules/auth/auth.repo.js'
 
 export default function configureGoogleStrategy() {
     const googleStrategy = new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID || '',
         clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-        callbackURL: '/auth/google/callback'
+        callbackURL: `${process.env.FRONTEND_URL}/auth/google/callback`
     }, async (accessToken, refreshToken, profile: any, done) => {
         try {
             let user = await authModel.findOne({
                 loginProvider: "Google",
-                loginId: profile.id,
+                email: Array.isArray(profile.emails) ? profile.emails[0].value : profile.email,
                 isDeleted: false,
             })
 
+            if (user) {
+                // check login ID for the sake of security
+                const isIdValid = await AuthRepo.compareLoginIds(profile.id, user.loginId ?? '')
+                if (!isIdValid) {
+                    user=null
+                }
+            }
+
+            // This part of if statement cannot be replaced with 'else' block because the security and this part both are independent
             if (!user) {
                 user = await authModel.create(
                     {
-                        _id: profile.id,
-                        firstName: profile.name.giverName,
-                        lastName: profile.name.familyName,
+                        loginId: await AuthRepo.encryptLoginId(profile.id),
+                        firstName: profile.name?.givenName,
+                        lastName: profile.name?.familyName,
                         email: Array.isArray(profile.emails) ? profile.emails[0].value : profile.emails,
                         profilePicture: Array.isArray(profile.photos) ? profile.photos[0].value : profile.photos,
                         loginProvider: "Google",
@@ -28,10 +38,10 @@ export default function configureGoogleStrategy() {
                     }
                 )
             }
+
             
 
-            // return done(null, user)
-            return done(null, false)
+            return done(null, user)
         } catch (error) {
             return done(error, false)
         }
