352112803ddd62727c5c1f768368180bd8c57c73
Abhijit Sengupta <senguptaavijit6@gmail.com>
2025-12-03 10:13:15 +0530

    passport will now read profile as well as email, from Google

--- server/src/middlewares/auth/passport.middleware.ts
+++ server/src/middlewares/auth/passport.middleware.ts
@@ -1,4 +1,4 @@
 import passport from "passport";
 
 export const authenticateJWT = passport.authenticate("jwt", {session: false})
-export const authenticateGoogle = passport.authenticate("google", {scope: ['profile'] })
+export const authenticateGoogle = passport.authenticate("google", {scope: ['profile', 'email'] })
