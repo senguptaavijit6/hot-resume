a9f898a04fb26f24aafd806115b896ed2996ad1e
Abhijit Sengupta <senguptaavijit6@gmail.com>
2025-12-03 10:21:13 +0530

    google auth callback is updated, and register and checkMail API endpoints are created

--- server/src/router/app.routes.ts
+++ server/src/router/app.routes.ts
@@ -1,20 +1,16 @@
 import { Router } from "express";
 import authController from "../modules/auth/auth.controller.js";
-import { authenticateGoogle } from "../middlewares/auth/passport.middleware.js";
 import passport from "passport";
-import initializePassport from "../config/passport.js";
+import FileUploader from "../helper/fileUploader.js";
+
 
 const router = Router()
+const profilePicUploader = new FileUploader({ destination: "uploads/profile/", maxFileSize: 1024*1024*5, supportedFileTypes: ["jpeg", "jpg", "png", "gif", "ico", "webp"] })
 
 router.get("/", authController.testServer)
 router.get("/auth/google", passport.authenticate('google', {scope: ["profile", "email"]}))
-router.get("/auth/google/callback", passport.authenticate('google', { failureMessage: true, failureRedirect: `${process.env.FRONTEND_URL}/auth/login` }), (req, res) => {
-    console.log("getting");
-    
-    return res.status(200).send({
-        message: "Working"
-    })
-})
-
+router.get("/auth/google/callback", passport.authenticate('google', { failureMessage: true, failureRedirect: `${process.env.FRONTEND_URL}/auth/login` }), authController.googleAuthCallback)
+router.post("/auth/register", profilePicUploader.upload.single("profilePicture") , authController.registerUser)
+router.post("/auth/checkMail", authController.checkEmailFunctionality)
 
 export default router
